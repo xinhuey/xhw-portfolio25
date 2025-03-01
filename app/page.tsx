566848ba204298d986f4fb1ba4b4ccb28177366e
xinhuey <xinhuey.wong@gmail.com>
2025-03-01 15:57:01 -0500

    fixed torotate

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Page() {
           setLoopNum(loopNum + 1);
           setDelta(500);
       }
-  }, [loopNum, isDeleting, text, delta]);
+  }, [loopNum, isDeleting, text, delta, toRotate]);
   
   useEffect(() => {
       let ticker = setInterval(tick, delta);
